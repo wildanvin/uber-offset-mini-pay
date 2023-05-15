90519036147b11b6c14afab38df43ca6738fc3e5
wildanvin <danvin.jim@gmail.com>
2023-05-15 04:08:42 -0500

    App flow working

--- packages/nextjs/components/ToucanSDK.tsx
+++ packages/nextjs/components/ToucanSDK.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { parseEther } from "ethers/lib/utils.js";
 import ToucanClient from "toucan-sdk";
 import { useProvider, useSigner } from "wagmi";
@@ -18,30 +18,77 @@ const ToucanSDK: React.FC<Props> = ({ distance }) => {
 
   // we will store our return value here
   const [tco2address, setTco2address] = useState("");
+  const [isRoundTrip, setIsRoundTrip] = useState(false);
+  const [daysTraveled, setDaysTraveled] = useState(1);
+  const [kmToOffset, setKmToOffset] = useState(distance);
+  const [tokensToOffset, setTokenToOffset] = useState(0);
+  const tonesOfCO2ByKm = 0.0003;
+
+  useEffect(() => {
+    const kms = distance * (isRoundTrip ? daysTraveled * 2 : daysTraveled);
+    setKmToOffset(kms);
+    const tokens = Math.ceil(kms * tonesOfCO2ByKm);
+    setTokenToOffset(tokens);
+  }, [distance, isRoundTrip, daysTraveled]);
 
   const redeemPoolToken = async (): Promise<void> => {
-    const redeemedTokenAddress = await toucan.redeemAuto2("NCT", parseEther("1"));
+    const redeemedTokenAddress = await toucan.redeemAuto2("NCT", parseEther(tokensToOffset.toString()));
     redeemedTokenAddress && setTco2address(redeemedTokenAddress[0].address);
   };
 
   const retirePoolToken = async (): Promise<void> => {
-    tco2address.length && (await toucan.retire(parseEther("1.0"), tco2address));
+    tco2address.length && (await toucan.retire(parseEther(tokensToOffset.toString()), tco2address));
   };
 
   return (
     <>
-      {/* <h1 className="text-gray-700 py-2">{name}</h1> */}
+      <div className="flex items-center my-4 mt-10">
+        <label htmlFor="roundTrip" className="mr-2 text-gray-700">
+          Is it a round trip?
+        </label>
+        <div className="relative inline-block w-10 mr-2 align-middle select-none">
+          <input
+            type="checkbox"
+            name="roundTrip"
+            id="roundTrip"
+            checked={isRoundTrip}
+            onChange={e => setIsRoundTrip(e.target.checked)}
+            className="w-6 h-6 rounded-full bg-white border-4 cursor-pointer"
+          />
+        </div>
+      </div>
+
+      <div className="my-4">
+        <label htmlFor="daysTraveled" className="mr-2 text-gray-700">
+          Days traveled:
+        </label>
+        <input
+          type="number"
+          name="daysTraveled"
+          id="daysTraveled"
+          value={daysTraveled}
+          onChange={e => setDaysTraveled(parseInt(e.target.value))}
+          className="w-20 px-3 py-1 text-gray-700 border rounded-lg focus:outline-none focus:border-primary"
+        />
+      </div>
+
+      <div className="mt-4">
+        <p className="text-lg font-medium text-gray-700">
+          {kmToOffset.toLocaleString()} km would nedd {tokensToOffset} NCT tokens to retire
+        </p>
+      </div>
+
       <button
         className="inline-flex w-full justify-center rounded-lg border px-5 my-7 py-2  bg-primary text-white hover:bg-opacity-90"
         onClick={() => redeemPoolToken()}
       >
-        {`Redeem ${distance}`}
+        {`Redeem ${tokensToOffset} NCT`}
       </button>
       <button
         className="inline-flex w-full justify-center rounded-lg border px-5 my-5 py-2  bg-primary text-white hover:bg-opacity-90"
         onClick={() => retirePoolToken()}
       >
-        {"Retire Tokens"}
+        {`Retire ${tokensToOffset} TCO2`}
       </button>
     </>
   );
